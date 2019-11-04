ad8e53ca432c24e55d549d0a81c3cdf5348ee2d7
Michael Giebner <mikegiebner@gmail.com>
2019-11-04 17:11:00 -0500

    # Repo: REACT-Section-9 # Repo Description: Colt Steele's Modern React Bootcamp, Section 9, State Exercises # Chapter 64: Coin Flipper Exercise    Basic files are setup

---
+++ coinflipper/src/CoinFlipper.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import Coin from "./Coin"
+
+class CoinFlipper extends Component{
+   render(){
+      return(
+         <Coin />
+      )
+   }
+}
+
+export default CoinFlipper

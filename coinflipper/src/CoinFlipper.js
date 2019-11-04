a27fd3c5c7253a7ec22e74e7c7abbca2e0ece329
Michael Giebner <mikegiebner@gmail.com>
2019-11-04 18:36:38 -0500

    CoinFlipper exercise complete

--- coinflipper/src/CoinFlipper.js
+++ coinflipper/src/CoinFlipper.js
@@ -3,25 +3,39 @@ import Coin from "./Coin"
 
 class CoinFlipper extends Component{
 
-   static defaultProps = {
-      headImage: "https://tinyurl.com/react-coin-heads-jpg",
-      tailImage: "https://tinyurl.com/react-coin-tails-jpg"
-   }
-
    constructor(props){
       super(props)
       this.state = {
-
+         numHeads: 0,
+         numTails: 0,
+         numFlips: 0,
+         currentFace: null
       }
+      this.flipCoin = this.flipCoin.bind(this)
+   }
+
+   incrementHeads(curState){return {numHeads: curState.numHeads + 1, currentFace: "https://tinyurl.com/react-coin-heads-jpg"}}
+   incrementTails(curState){return {numTails: curState.numTails + 1, currentFace: "https://tinyurl.com/react-coin-tails-jpg"}}
+   incrementFlips(curState){return {numFlips: curState.numFlips + 1}}
+
+   flipCoin() {
+      var heads = Math.floor(Math.random() * 2)
+      if(heads){
+         this.setState(this.incrementHeads)
+      }
+      else {
+         this.setState(this.incrementTails)
+      }
+      this.setState(this.incrementFlips)
    }
 
    render(){
       return(
          <div>
             <h1>Let's flip a coin!</h1>
-            <Coin face={this.props.headImage}/>
-            <button>Flip Meee</button>
-            <h3>Out of 0 flips, there have been 0 heads and 0 tails</h3>
+            {this.state.currentFace !== null && <Coin face={this.state.currentFace}/> }
+            <button onClick={this.flipCoin}>Flip Meee</button>
+            <h3>Out of {this.state.numFlips} flips, there have been {this.state.numHeads} heads and {this.state.numTails} tails</h3>
          </div>
       )
    }
--- readme.md
+++ readme.md
@@ -6,3 +6,8 @@
 # Version 2:
    - Get basic structure of CoinFlipper.js setup
    - Get Coin.js to display a coin image (complete, but man does it need some css to make the coin smaller!!)
+
+# Version 3:
+   State added
+   Setup logic so that no image shows until after the first flip
+   
